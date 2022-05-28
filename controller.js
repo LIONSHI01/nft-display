7c5088928b6b83a8ba0e22f024b4386e087189f5
lionshi01 <lionshi2012@gmail.com>
2022-05-28 09:05:12 +0800

    modify helper module

--- controller.js
+++ controller.js
@@ -1,1 +1,4 @@
+import { AJAX } from "./helper.js";
 
+const wallet = "0xA90c70882Fc63ac514bE15743a13595Cb39F767D";
+AJAX(wallet);
--- helper.js
+++ helper.js
@@ -1,8 +1,14 @@
-export const AJAX = async function (url) {
+export const AJAX = async function (wallet) {
   try {
-    const response = await fetch(url);
+    const options = { method: "GET", headers: { Accept: "application/json" } };
+
+    const url = `https://api.opensea.io/api/v1/assets?owner=${wallet}&order_direction=desc&limit=20&include_orders=false`;
+
+    const response = await fetch(url, options);
     const data = await response.json();
-    if (!response.ok) throw new Error(`${data.message}${response.status}`);
+
+    if (!response.ok) throw new Error(`${data.message} ${response.status}`);
+    console.log(data);
     return data;
   } catch (err) {
     throw err;
